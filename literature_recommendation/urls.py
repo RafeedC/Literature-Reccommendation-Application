012ea52a68499660082a24355976c860f314b790
Rafeed Choudhury <rafeedchoudhury@gmail.com>
2023-11-07 21:59:30 -0500

    Added 5 different pages to the app, a navigation bar and also a login and signup page. Used css styling and changed the structure of the files within the project.

--- literature_recommendation/urls.py
+++ literature_recommendation/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
-from .views import homePageView
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
 
 urlpatterns = [
-    path('', homePageView, name='home')
+    path('', views.home, name='home'),
+    path('book_swiping/', views.book_swiping, name='book_swiping'),
+    path('my_list/', views.my_list, name='my_list'),
+    path('my_likes/', views.my_likes, name='my_likes'),
+    path('my_profile/', views.my_profile, name='my_profile'),
+    path('login/', LoginView.as_view(template_name='literature_recommendation/login.html'), name='login'),  # Use the built-in view
+    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),  # Use the built-in view
+    path('signup/', views.SignUpView.as_view(), name='signup'),  # Your custom sign-up view
+
+
 ]
--- literature_recommendation/views.py
+++ literature_recommendation/views.py
@@ -1,6 +1,32 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout
 
+def home(request):
+    return render(request, 'literature_recommendation/home.html')
 
-def homePageView(request):
-    return HttpResponse('Hello, World!')
+def book_swiping(request):
+    return render(request, 'literature_recommendation/book_swiping.html')
+
+def my_list(request):
+    return render(request, 'literature_recommendation/my_list.html')
+
+def my_likes(request):
+    return render(request, 'literature_recommendation/my_likes.html')
+
+def my_profile(request):
+    return render(request, 'literature_recommendation/my_profile.html')
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')  # Redirect to login page after sign up
+    template_name = 'literature_recommendation/signup.html'  # Your sign-up template
+
+    def form_valid(self, form):
+        # Save the new user first
+        user = form.save()
+        # Then log the user in
+        login(self.request, user)
+        return redirect('home')  # Redirect to home page after sign up
